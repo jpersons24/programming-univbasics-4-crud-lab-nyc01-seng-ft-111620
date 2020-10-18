711b1ed9925a4e11d2160b47d4cfde83367976ad
Jakob Persons <jakobep24@gmail.com>
2020-10-18 18:14:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -35,5 +35,7 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  element = array[index_number]
+  first_array = array.delete_at(index_number)
+  updated_array = first_array << element
+  updated_array
 end
